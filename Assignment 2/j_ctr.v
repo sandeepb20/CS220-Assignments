ff3bbb9d497a744bae54cd167ae3d2ccf45e2cb6
mannemr20 <82554853+mannemr20@users.noreply.github.com>
2022-02-06 15:51:58 +0530

    Add files via upload

---
+++ Assignment 2/j_ctr.v
@@ -0,0 +1,12 @@
+module j_ctr(clk,clr,q);
+input clk,clr;
+output reg [7:0] q;
+always @(posedge clk) begin
+    if(!clr) begin
+        q=00000000;
+    end
+    else begin
+        q={~q[0],q[7:1]};
+    end
+end
+endmodule
---
+++ Assignment 2/j_ctr_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns/1ns
+`include "j_ctr.v"
+
+module j_ctr_tb;
+
+reg clk=0,clr=0;
+wire [7:0] q;
+
+j_ctr uut(clk,clr,q);
+
+always begin
+    #10;
+    clk=~clk;
+end
+
+initial begin
+    $dumpfile("j_ctr_tb.vcd");
+    $dumpvars(0,j_ctr_tb);
+    $monitor("T=%0t q=%b", $time, q);  
+    repeat (2) @(posedge clk);  
+    clr <= 1;  
+    repeat (15) @(posedge clk);
+    $finish;  
+end  
+
+endmodule
